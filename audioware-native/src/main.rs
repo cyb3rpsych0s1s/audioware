0f9cf47d477b1ee53f31a636db020ce96a27e965
Roms1383 <romain.kelifa@gmail.com>
2023-08-03 08:22:30 +0700

    :alembic: keeping a ref around

--- audioware-native/src/main.rs
+++ audioware-native/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::time::Duration;
+use std::{cell::RefCell, path::Path, time::Duration};
 
 use kira::{
     clock::ClockSpeed,
@@ -13,6 +13,11 @@ use kira::{
     tween::Tween,
     Volume,
 };
+use metadata::MediaFileMetadata;
+
+thread_local! {
+  static MANAGER: RefCell<AudioManager<DefaultBackend>> = RefCell::new(AudioManager::new(AudioManagerSettings::default()).expect("audio manager"));
+}
 
 pub fn first_test() -> Result<(), anyhow::Error> {
     let mut manager = AudioManager::<DefaultBackend>::new(AudioManagerSettings::default())?;
@@ -84,61 +89,90 @@ pub fn second_test() -> Result<(), anyhow::Error> {
 }
 
 pub fn third_test() -> Result<(), anyhow::Error> {
-    let mut manager = AudioManager::<DefaultBackend>::new(AudioManagerSettings::default())?;
-    let reverb = manager.add_sub_track({
-        let mut builder = TrackBuilder::new();
-        builder.add_effect(ReverbBuilder::new().mix(1.0));
-        builder.add_effect(
-            FilterBuilder::new()
-                .mode(kira::track::effect::filter::FilterMode::LowPass)
-                .resonance(1.0),
-        );
-        builder
-    })?;
-    let clock = manager.add_clock(ClockSpeed::TicksPerSecond(10.))?;
-    let more = manager.add_sub_track({
-        let mut builder = TrackBuilder::new().routes(TrackRoutes::new().with_route(&reverb, 0.8));
-        builder.add_effect(DelayBuilder::new().delay_time(0.3));
-        builder
-    })?;
-    let less = manager
-        .add_sub_track(TrackBuilder::new().routes(TrackRoutes::new().with_route(&reverb, 0.4)))?;
-    let as_if_i_didnt_know_already = StaticSoundData::from_file(
-        "fem_v_aiidka.wav",
-        StaticSoundSettings::new()
-            .output_destination(&less)
-            .start_time(clock.time())
-            .panning(0.0),
-    )?;
-    let mut aiidka = manager.play(as_if_i_didnt_know_already)?;
-    aiidka.set_playback_rate(
-        3.0,
-        Tween {
-            duration: Duration::from_secs(3),
-            start_time: kira::StartTime::ClockTime(clock.time() + 5),
-            easing: kira::tween::Easing::InPowi(2),
-        },
-    )?;
-    let come_on_biomon_cant_you_just_give_me_a_break = StaticSoundData::from_file(
-        "fem_v_cobcygmab_02.wav",
-        StaticSoundSettings::new()
-            .output_destination(&more)
-            .start_time(clock.time() + 5)
-            .panning(1.0),
-    )?;
-    let mut cobcyjgmeab = manager.play(come_on_biomon_cant_you_just_give_me_a_break)?;
-    cobcyjgmeab.set_playback_rate(
-        2.0,
-        Tween {
-            duration: Duration::from_secs(3),
-            start_time: kira::StartTime::ClockTime(clock.time() + 15),
-            ..Default::default()
-        },
-    )?;
-    clock.start()?;
-    std::thread::sleep(Duration::from_secs(10));
+    MANAGER.with(|cell| {
+        println!("in cell handler");
+        let mut manager = cell.borrow_mut();
+        let reverb = manager
+            .add_sub_track({
+                let mut builder = TrackBuilder::new();
+                builder.add_effect(ReverbBuilder::new().mix(1.0));
+                builder.add_effect(
+                    FilterBuilder::new()
+                        .mode(kira::track::effect::filter::FilterMode::LowPass)
+                        .resonance(1.0),
+                );
+                builder
+            })
+            .expect("reverb");
+        let clock = manager
+            .add_clock(ClockSpeed::TicksPerSecond(10.))
+            .expect("clock");
+        let more = manager
+            .add_sub_track({
+                let mut builder =
+                    TrackBuilder::new().routes(TrackRoutes::new().with_route(&reverb, 0.8));
+                builder.add_effect(DelayBuilder::new().delay_time(0.3));
+                builder
+            })
+            .expect("more");
+        let less = manager
+            .add_sub_track(TrackBuilder::new().routes(TrackRoutes::new().with_route(&reverb, 0.4)))
+            .expect("less");
+        let as_if_i_didnt_know_already = StaticSoundData::from_file(
+            "fem_v_aiidka.wav",
+            StaticSoundSettings::new()
+                .output_destination(&less)
+                .start_time(clock.time())
+                .panning(0.0),
+        )
+        .expect("first sound");
+        let duration_aiidka = MediaFileMetadata::new(&Path::new("fem_v_aiidka.wav"))
+            .expect("metadata")
+            ._duration;
+        println!("aiidka lasts for: {duration_aiidka:#?}");
+        let mut aiidka = manager.play(as_if_i_didnt_know_already).expect("play");
+        aiidka
+            .set_playback_rate(
+                3.0,
+                Tween {
+                    duration: Duration::from_secs(3),
+                    start_time: kira::StartTime::ClockTime(clock.time() + 5),
+                    easing: kira::tween::Easing::InPowi(2),
+                },
+            )
+            .expect("set playback rate");
+        let come_on_biomon_cant_you_just_give_me_a_break = StaticSoundData::from_file(
+            "fem_v_cobcygmab_02.wav",
+            StaticSoundSettings::new()
+                .output_destination(&more)
+                .start_time(clock.time() + 5)
+                .panning(1.0),
+        )
+        .expect("second sound");
+        let duration_cobcyjgmeab = MediaFileMetadata::new(&Path::new("fem_v_cobcygmab_02.wav"))
+            .expect("metadata")
+            ._duration;
+        println!("cobcyjgmeab lasts for: {duration_cobcyjgmeab:#?}");
+        let mut cobcyjgmeab = manager
+            .play(come_on_biomon_cant_you_just_give_me_a_break)
+            .expect("play");
+        cobcyjgmeab
+            .set_playback_rate(
+                2.0,
+                Tween {
+                    duration: Duration::from_secs(3),
+                    start_time: kira::StartTime::ClockTime(clock.time() + 15),
+                    ..Default::default()
+                },
+            )
+            .expect("set playback rate");
+        clock.start().expect("start clock");
+        std::thread::sleep(Duration::from_secs(10));
+    });
     Ok(())
 }
+
+fn initialize() {}
 
 pub fn main() -> Result<(), anyhow::Error> {
     third_test()
