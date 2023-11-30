98a2b15db47d9e4ae089d2106791252236ba46e8
Roms1383 <romain.kelifa@gmail.com>
2023-11-30 11:33:57 +0700

    :memo: add memory addresses docs

--- audioware/src/addresses.rs
+++ audioware/src/addresses.rs
@@ -1,4 +1,6 @@
+/// memory address for internal event handler related to [RED4ext::ent::AudioEvent](https://github.com/WopsS/RED4ext.SDK/blob/master/include/RED4ext/Scripting/Natives/Generated/ent/AudioEvent.hpp#L18)
 pub(crate) const ON_ENT_AUDIO_EVENT: usize = 0x05F7DC8;
 pub(crate) const ON_MUSIC_EVENT: usize = 0x2476F68;
 pub(crate) const ON_VOICE_EVENT: usize = 0x08205FC;
+/// memory address for [AudioSystem::Play](https://codeberg.org/adamsmasher/cyberpunk/src/branch/master/core/systems/audioSystem.swift#L10)
 pub(crate) const ON_AUDIOSYSTEM_PLAY: usize = 0x02AF3F8;
