3cfafb47c126281d694813873519bc7e7f92c5c3
Roms1383 <romain.kelifa@gmail.com>
2023-12-05 14:38:34 +0700

    :construction: add sub tracks and spatial scene

--- audioware/src/engine.rs
+++ audioware/src/engine.rs
@@ -1,11 +1,13 @@
 use std::{
     borrow::BorrowMut,
+    collections::HashMap,
     sync::{Arc, Mutex},
 };
 
 use kira::{
     manager::{backend::DefaultBackend, AudioManager, AudioManagerSettings},
-    track::TrackBuilder,
+    spatial::scene::{SpatialSceneHandle, SpatialSceneSettings},
+    track::{effect::reverb::ReverbBuilder, TrackBuilder, TrackHandle, TrackId, TrackRoutes},
 };
 use lazy_static::lazy_static;
 
@@ -13,17 +15,61 @@ use crate::Audioware;
 
 lazy_static! {
     static ref AUDIO: Arc<Mutex<Audioware>> = Arc::new(Mutex::new(Audioware::default()));
+    static ref TRACKS: Arc<Mutex<HashMap<NamedTrackId, TrackHandle>>> =
+        Arc::new(Mutex::new(HashMap::default()));
+    static ref SCENE: Arc<Mutex<Option<SpatialSceneHandle>>> = Arc::new(Mutex::new(None));
+}
+
+#[derive(Debug)]
+pub struct NamedTrackId(&'static str, TrackId);
+
+impl PartialEq for NamedTrackId {
+    fn eq(&self, other: &Self) -> bool {
+        self.1 == other.1
+    }
+}
+impl Eq for NamedTrackId {}
+impl std::hash::Hash for NamedTrackId {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.1.hash(state);
+    }
 }
 
 impl Audioware {
     fn setup() -> anyhow::Result<AudioManager> {
         let mut manager = AudioManager::<DefaultBackend>::new(AudioManagerSettings::default())?;
-        let vocal = TrackBuilder::default();
-        let mental = TrackBuilder::default();
-        let emission = TrackBuilder::default();
-        manager.add_sub_track(vocal)?;
-        manager.add_sub_track(mental)?;
-        manager.add_sub_track(emission)?;
+        let reverb = manager.add_sub_track({
+            let mut builder = TrackBuilder::new();
+            builder.add_effect(ReverbBuilder::new().mix(1.0));
+            builder
+        })?;
+        let ambience = manager.add_sub_track(
+            TrackBuilder::new().routes(TrackRoutes::new().with_route(&reverb, 0.5)),
+        )?;
+        let player = manager.add_sub_track(
+            TrackBuilder::new().routes(TrackRoutes::new().with_route(&reverb, 0.25)),
+        )?;
+        let vocal = manager.add_sub_track(
+            TrackBuilder::new().routes(TrackRoutes::new().with_route(&player, 1.)),
+        )?;
+        let mental = manager.add_sub_track(
+            TrackBuilder::new().routes(TrackRoutes::new().with_route(&player, 1.)),
+        )?;
+        let emissive = manager.add_sub_track(
+            TrackBuilder::new().routes(TrackRoutes::new().with_route(&player, 1.)),
+        )?;
+        let environment = manager.add_spatial_scene(SpatialSceneSettings::default())?;
+
+        if let Ok(mut guard) = TRACKS.clone().borrow_mut().try_lock() {
+            guard.insert(NamedTrackId("ambience", ambience.id()), ambience);
+            guard.insert(NamedTrackId("player", player.id()), player);
+            guard.insert(NamedTrackId("player:vocal", vocal.id()), vocal);
+            guard.insert(NamedTrackId("player:emissive", emissive.id()), emissive);
+            guard.insert(NamedTrackId("player:mental", mental.id()), mental);
+        }
+        if let Ok(mut guard) = SCENE.clone().borrow_mut().try_lock() {
+            *guard = Some(environment);
+        }
         Ok(manager)
     }
     pub(crate) fn create() -> anyhow::Result<()> {
