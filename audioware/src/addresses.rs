590dd7fb0af11f7c804acba94c0d2decb770ffbb
Roms1383 <romain.kelifa@gmail.com>
2024-01-24 20:28:15 +0700

    :bulb: comment audio system switch address

--- audioware/src/addresses.rs
+++ audioware/src/addresses.rs
@@ -4,4 +4,5 @@ const IMAGE_BASE: usize = 0x140000000;
 pub const ON_AUDIOSYSTEM_PLAY: usize = 0x140975FE4 - IMAGE_BASE;
 /// memory address for [AudioSystem::Stop](https://codeberg.org/adamsmasher/cyberpunk/src/branch/master/core/systems/audioSystem.swift#L12)
 pub const ON_AUDIOSYSTEM_STOP: usize = 0x142440C30 - IMAGE_BASE;
+/// memory address for [AudioSystem::Switch](https://codeberg.org/adamsmasher/cyberpunk/src/branch/master/core/systems/audioSystem.swift#L14)
 pub const ON_AUDIOSYSTEM_SWITCH: usize = 0x140690804 - IMAGE_BASE;
