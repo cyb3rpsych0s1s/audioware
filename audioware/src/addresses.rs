53309c163c8bd323219d46e5df955d8b57c30d73
Roms1383 <romain.kelifa@gmail.com>
2024-02-01 18:25:13 +0700

    :alien: update addresses

--- audioware/src/addresses.rs
+++ audioware/src/addresses.rs
@@ -1,8 +1,8 @@
 const IMAGE_BASE: usize = 0x140000000;
 
 /// memory address for [AudioSystem::Play](https://codeberg.org/adamsmasher/cyberpunk/src/branch/master/core/systems/audioSystem.swift#L10)
-pub const ON_AUDIOSYSTEM_PLAY: usize = 0x140975FE4 - IMAGE_BASE;
+pub const ON_AUDIOSYSTEM_PLAY: usize = 0x14096D8B8 - IMAGE_BASE;
 /// memory address for [AudioSystem::Stop](https://codeberg.org/adamsmasher/cyberpunk/src/branch/master/core/systems/audioSystem.swift#L12)
-pub const ON_AUDIOSYSTEM_STOP: usize = 0x142440C30 - IMAGE_BASE;
+pub const ON_AUDIOSYSTEM_STOP: usize = 0x14244FDA4 - IMAGE_BASE;
 /// memory address for [AudioSystem::Switch](https://codeberg.org/adamsmasher/cyberpunk/src/branch/master/core/systems/audioSystem.swift#L14)
-pub const ON_AUDIOSYSTEM_SWITCH: usize = 0x140690804 - IMAGE_BASE;
+pub const ON_AUDIOSYSTEM_SWITCH: usize = 0x140291288 - IMAGE_BASE;
