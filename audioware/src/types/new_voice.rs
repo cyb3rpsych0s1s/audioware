52e74db3a671c27bde10c962c859b79ea04c0ea0
Roms1383 <romain.kelifa@gmail.com>
2024-03-09 17:22:44 +0700

    :white_check_mark: add test for new voices format

--- audioware/src/types/new_voice.rs
+++ audioware/src/types/new_voice.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::collections::HashMap;
 
 use fixed_map::Map;
@@ -19,9 +21,11 @@ pub struct Voices {
 }
 
 #[derive(Debug, Clone, Deserialize)]
-pub struct LocalizedVoices(Map<Locale, Voice>);
+#[serde(transparent)]
+pub struct LocalizedVoices(pub Map<Locale, Voice>);
 
 #[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
 pub enum Voice {
     Neutral(NeutralVoice),
     Genderized(GenderizedVoice),
@@ -30,6 +34,7 @@ pub enum Voice {
 #[derive(Debug, Clone, Deserialize, Validate)]
 pub struct NeutralVoice {
     #[validate(custom(function = "validate_static_sound_data", arg = "&'v_a std::path::Path"))]
+    #[serde(rename = "fem")]
     female: Option<std::path::PathBuf>,
     #[validate(custom(function = "validate_static_sound_data", arg = "&'v_a std::path::Path"))]
     male: Option<std::path::PathBuf>,
@@ -38,6 +43,7 @@ pub struct NeutralVoice {
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct GenderizedVoice {
+    #[serde(rename = "fem")]
     female: AudioSubtitle,
     male: AudioSubtitle,
 }
@@ -70,5 +76,40 @@ impl Voices {
 impl GenderizedVoice {
     pub fn subtitle(&self) -> (&str, &str) {
         (&self.female.subtitle, &self.male.subtitle)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use validator::ValidateArgs;
+
+    use crate::types::new_voice::{NeutralVoice, Voices};
+
+    #[test]
+    pub fn deserialize() {
+        let filepath = std::path::PathBuf::from("./tests/voices2.yml");
+        let yaml = std::fs::read(filepath).unwrap();
+        let voices = serde_yaml::from_slice::<Voices>(yaml.as_slice());
+        assert!(voices.is_ok());
+    }
+
+    #[test]
+    pub fn validate() {
+        let folder = std::path::PathBuf::from("./tests");
+        let audio = NeutralVoice {
+            female: Some("en-us/v_sq017_f_19795c050029f000.Wav".into()),
+            male: Some("en-us/v_sq017_f_19795c050029f000.Wav".into()),
+            subtitle: "Again?".to_string(),
+        };
+        let validation = audio.validate_args((folder.as_path(), folder.as_path()));
+        assert!(validation.is_ok());
+
+        let audio = NeutralVoice {
+            female: Some("en-us/../../v_sq017_f_19795c050029f000.Wav".into()),
+            male: Some("en-us/../../v_sq017_f_19795c050029f000.Wav".into()),
+            subtitle: "Again?".to_string(),
+        };
+        let validation = audio.validate_args((folder.as_path(), folder.as_path()));
+        assert!(validation.is_err());
     }
 }
