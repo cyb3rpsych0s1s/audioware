c73630e89565f5638d366a360dd782136489fa76
Roms1383 <romain.kelifa@gmail.com>
2024-03-29 20:38:33 +0700

    :bug: fix reverb

--- audioware/src/engine/tracks.rs
+++ audioware/src/engine/tracks.rs
@@ -86,7 +86,7 @@ pub fn setup(manager: &mut AudioManager) -> anyhow::Result<()> {
     })?;
     let mut scene = manager.add_spatial_scene(SpatialSceneSettings::default())?;
     let main = manager
-        .add_sub_track(TrackBuilder::new().routes(TrackRoutes::new().with_route(&reverb, 0.25)))?;
+        .add_sub_track(TrackBuilder::new().routes(TrackRoutes::new().with_route(&reverb, 0.)))?;
     let v = scene.add_listener(
         Vec3::ZERO,
         Quat::IDENTITY,
