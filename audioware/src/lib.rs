28bed1c1fd889e77d9e7f9485661d571c8d2beb5
Roms1383 <romain.kelifa@gmail.com>
2024-04-03 13:53:59 +0700

    :bookmark: bump version

--- audioware/src/lib.rs
+++ audioware/src/lib.rs
@@ -28,7 +28,7 @@ impl IsValid for CName {
 struct Audioware;
 
 impl Plugin for Audioware {
-    const VERSION: Version = Version::new(0, 8, 2);
+    const VERSION: Version = Version::new(0, 8, 3);
 
     fn register() {
         red4ext_rs::info!("on register audioware");
