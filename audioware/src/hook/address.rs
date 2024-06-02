7fe26f51d308aa46c673ff4beb5086da4b973c70
Roms1383 <romain.kelifa@gmail.com>
2024-06-02 16:26:38 +0700

    :bug: fix incorrect address

--- audioware/src/hook/address.rs
+++ audioware/src/hook/address.rs
@@ -3,4 +3,4 @@ const IMAGE_BASE: usize = 0x140000000;
 /// memory address for [AudioSystem::Play](https://codeberg.org/adamsmasher/cyberpunk/src/branch/master/core/systems/audioSystem.swift#L10)
 pub const ON_AUDIOSYSTEM_PLAY: usize = 0x140974F58 - IMAGE_BASE;
 /// memory address for [AudioSystem::Stop](https://codeberg.org/adamsmasher/cyberpunk/src/branch/master/core/systems/audioSystem.swift#L12)
-pub const ON_AUDIOSYSTEM_STOP: usize = 0x1424503C8 - IMAGE_BASE;
+pub const ON_AUDIOSYSTEM_STOP: usize = 0x1424503F8 - IMAGE_BASE;
