1fc8593df57380cd0a766d4b6830a161fcdfd2a1
Roms1383 <romain.kelifa@gmail.com>
2024-07-11 10:32:43 +0700

    :recycle: use bindings definition

--- audioware/src/types.rs
+++ audioware/src/types.rs
@@ -1,16 +1,31 @@
+use std::fmt::Debug;
+
 use red4rs::types::{Native, ScriptClass};
 
-#[derive(Debug, Clone)]
-#[repr(C)]
-pub struct ScriptableSystem {
-    pub _padding0: [u8; 0x530],
+#[repr(transparent)]
+pub struct ScriptableSystem(red4rs_bindings::ScriptableSystem);
+
+impl Debug for ScriptableSystem {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("ScriptableSystem")
+            .field(&self.0._padding0)
+            .finish()
+    }
 }
 
 impl Default for ScriptableSystem {
     fn default() -> Self {
-        Self {
+        Self(red4rs_bindings::ScriptableSystem {
             _padding0: [0; 0x530],
-        }
+        })
+    }
+}
+
+impl Clone for ScriptableSystem {
+    fn clone(&self) -> Self {
+        Self(red4rs_bindings::ScriptableSystem {
+            _padding0: self.0._padding0,
+        })
     }
 }
 
