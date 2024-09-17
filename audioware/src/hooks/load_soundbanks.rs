41814755bd1cf3605f59ba8a63b786a9c75c6e7e
Roms1383 <romain.kelifa@gmail.com>
2024-09-17 15:31:19 +0700

    :bug: fix intermittent crash

--- audioware/src/hooks/load_soundbanks.rs
+++ audioware/src/hooks/load_soundbanks.rs
@@ -3,9 +3,10 @@
 use audioware_bank::BNKS;
 use audioware_manifest::SoundBankInfo;
 use red4ext_rs::{
-    addr_hashes, hooks,
-    types::{CName, CNamePool, RedHashMap, Ref},
-    SdkEnv,
+    addr_hashes::{self, resolve},
+    hooks,
+    types::{CName, CNamePool, IScriptable, ISerializable, RedHashMap, Ref},
+    RttiSystemMut, SdkEnv,
 };
 
 hooks! {
@@ -24,29 +25,27 @@ pub fn attach_hook(env: &SdkEnv) {
 unsafe extern "C" fn detour(a1: i64, cb: unsafe extern "C" fn(a1: i64) -> bool) -> bool {
     crate::utils::lifecycle!("LoadSoundBanks called");
     let res = cb(a1);
-    crate::utils::lifecycle!("LoadSoundBanks: before transmuting");
-    let map = a1 + 104;
-    let map = map as *mut red4ext_rs::types::RedHashMap<
-        red4ext_rs::types::CName,
-        red4ext_rs::types::Ref<audioware_bank::SoundBankInfo>,
-    >;
-    let map: &mut RedHashMap<CName, Ref<audioware_bank::SoundBankInfo>> = unsafe { &mut *map };
-    crate::utils::lifecycle!(
-        "LoadSoundBanks: after transmuting, map size({})/capacity({})",
-        map.size(),
-        map.capacity()
-    );
-    for (key, value) in BNKS.iter() {
-        let reference: Ref<audioware_bank::SoundBankInfo> =
-            Ref::new_with(|x: &mut audioware_bank::SoundBankInfo| {
-                x.name = value.name;
-                x.is_resident = value.is_resident;
-                x.path = value.path.clone();
-            })
-            .unwrap();
-        crate::utils::lifecycle!("LoadSoundBanks: before insertion");
-        let _ = map.insert(*key, reference);
-        crate::utils::lifecycle!("LoadSoundBanks: after insertion");
+
+    let map = (a1 + 104) as i128;
+    let map = unsafe { std::mem::transmute::<i128, Ref<ISerializable>>(map) };
+    if let Some(map) = map.clone().fields_mut() {
+        let map = unsafe {
+            std::mem::transmute::<
+                &mut ISerializable,
+                &mut RedHashMap<CName, Ref<audioware_bank::SoundBankInfo>>,
+            >(map)
+        };
+        for (key, value) in BNKS.iter() {
+            let reference: Ref<audioware_bank::SoundBankInfo> =
+                Ref::new_with(|x: &mut audioware_bank::SoundBankInfo| {
+                    x.name = value.name;
+                    x.is_resident = value.is_resident;
+                    x.path = value.path.clone();
+                })
+                .unwrap();
+            let existing = map.insert(*key, reference.clone());
+        }
     }
+
     res
 }
