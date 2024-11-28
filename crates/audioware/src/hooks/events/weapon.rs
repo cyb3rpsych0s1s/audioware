238bbfcc46ae603d553f30b6be53204e1367b563
Roms1383 <romain.kelifa@gmail.com>
2024-11-28 22:32:11 +0700

    :monocle_face: stop weapon fire event

--- crates/audioware/src/hooks/events/weapon.rs
+++ crates/audioware/src/hooks/events/weapon.rs
@@ -6,6 +6,7 @@ pub fn attach_hook(env: &red4ext_rs::SdkEnv) {
 mod pre_fire {
     use crate::{attach_native_event, PreFireEvent};
     use red4ext_rs::types::IScriptable;
+
     use red4ext_rs::ScriptClass;
 
     attach_native_event!(
@@ -26,23 +27,37 @@ mod pre_fire {
 }
 
 mod stop_firing {
-    use crate::{attach_native_event, StopFiringEvent};
+    use crate::{attach_native_event, Event, StopFiringEvent, StopWeaponFire};
+    use red4ext_rs::types::CName;
     use red4ext_rs::types::IScriptable;
     use red4ext_rs::ScriptClass;
 
     attach_native_event!(
         super::super::super::offsets::WEAPON_STOP_FIRING_EVENT,
-        crate::StopFiringEvent
+        crate::Event
     );
 
     unsafe extern "C" fn detour(
         a1: *mut IScriptable,
-        a2: *mut StopFiringEvent,
-        cb: unsafe extern "C" fn(a1: *mut IScriptable, a2: *mut StopFiringEvent),
+        a2: *mut Event,
+        cb: unsafe extern "C" fn(a1: *mut IScriptable, a2: *mut Event),
     ) {
         let event = &*a2;
-        let StopFiringEvent { .. } = event;
-        crate::utils::lifecycle!("intercepted {}", StopFiringEvent::NAME);
+        if event
+            .as_ref()
+            .class()
+            .name()
+            .eq(&CName::new(StopFiringEvent::NAME))
+        {
+            crate::utils::lifecycle!("intercepted {}", StopFiringEvent::NAME);
+        } else if event
+            .as_ref()
+            .class()
+            .name()
+            .eq(&CName::new(StopWeaponFire::NAME))
+        {
+            crate::utils::lifecycle!("intercepted {}", StopWeaponFire::NAME);
+        }
         cb(a1, a2);
     }
 }
