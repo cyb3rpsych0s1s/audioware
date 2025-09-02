28add56b917f4370d1112e38cedba11426596e48
Roms1383 <romain.kelifa@gmail.com>
2025-09-02 12:32:49 +0700

    :white_check_mark: scene dialog

--- crates/manifest/src/de/dialog.rs
+++ crates/manifest/src/de/dialog.rs
@@ -69,19 +69,75 @@ impl From<SceneDialog> for AnySceneDialog {
     }
 }
 
-impl From<SceneDialog> for Audio {
-    fn from(value: SceneDialog) -> Self {
-        match value {
-            SceneDialog::SingleInline {
-                dialogs,
-                usage,
-                settings,
-            } => todo!(),
-            SceneDialog::DualInline {
-                dialogs,
-                usage,
-                settings,
-            } => todo!(),
+#[cfg(test)]
+mod tests {
+    mod inline_dialog {
+        use super::super::SceneDialog;
+        use std::collections::HashMap;
+        use test_case::test_case;
+
+        #[test_case(r##"1300277703738512077:
+    en-us: ./somewhere/dialog.wav"## ; "implicit on-demand inline scene dialog")]
+        #[test_case(r##"1300277703738512077:
+    en-us: ./somewhere/dialog.wav
+    fr-fr: ./somewhere/else/dialog.wav"## ; "format must be consistent across locales")]
+        fn basic_format(yaml: &str) {
+            let unique_dialog = serde_yaml::from_str::<HashMap<i64, SceneDialog>>(yaml);
+            dbg!("{}", &unique_dialog);
+            assert!(unique_dialog.is_ok());
+        }
+
+        #[test_case(r##"1300277703738512077:
+    en-us:
+        file: ./somewhere/dialog.wav
+    fr-fr: ./somewhere/else/dialog.wav"## ; "format must be consistent")]
+        fn incompatibility(yaml: &str) {
+            let unique_dialog = serde_yaml::from_str::<HashMap<i64, SceneDialog>>(yaml);
+            dbg!("{}", &unique_dialog);
+            assert!(unique_dialog.is_err());
+        }
+    }
+    mod dual_dialog {
+        use std::collections::HashMap;
+
+        use super::super::SceneDialog;
+        use test_case::test_case;
+
+        #[test_case(r##"1300277703738512077:
+    en-us:
+        fem: ./somewhere/dialog.wav
+        male: ./somewhere/else/dialog.wav
+    fr-fr:
+        fem: ./elsewhere/dialog.wav
+        male: ./elsewhere/else/dialog.wav"## ; "dual scene dialog")]
+        #[test_case(r##"1300277703738512077:
+    en-us:
+        fem: ./somewhere/dialog.wav
+        male: ./somewhere/else/dialog.wav
+    fr-fr:
+        fem: ./elsewhere/dialog.wav
+        male: ./elsewhere/else/dialog.wav
+    settings:
+        region:
+            starts: 500ms"## ; "dual scene dialog with specific settings")]
+        fn basic_format_with_settings(yaml: &str) {
+            let dual_dialog = serde_yaml::from_str::<HashMap<i64, SceneDialog>>(yaml);
+            dbg!("{}", &dual_dialog);
+            assert!(dual_dialog.is_ok());
+        }
+
+        #[test_case(r##"1300277703738512077:
+    en-us:
+        fem: ./somewhere/dialog.wav"## ; "format must be consistent, partially defined gender is not allowed")]
+        #[test_case(r##"1300277703738512077:
+    en-us:
+        fem: ./fem/dialog.wav
+        male: ./male/dialog.wav
+    fr-fr: ./unique/dialog.wav"## ; "format must be consistent, mixing unique and gender-based is not allowed")]
+        fn incompatibility(yaml: &str) {
+            let dual_dialog = serde_yaml::from_str::<HashMap<i64, SceneDialog>>(yaml);
+            dbg!("{}", &dual_dialog);
+            assert!(dual_dialog.is_err());
         }
     }
 }
