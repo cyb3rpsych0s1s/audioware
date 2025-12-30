6e3fb710c6e365c0523c53aaf584cf7b7c853f5e
Roms1383 <romain.kelifa@gmail.com>
2025-12-30 17:43:29 +0700

    :label: set volume trait

--- crates/audioware/src/engine/traits/volume.rs
+++ crates/audioware/src/engine/traits/volume.rs
@@ -1,8 +1,6 @@
 use audioware_core::Amplitude;
 use kira::Tween;
 
-use crate::engine::traits::AnyHandle;
-
 pub trait SetVolume {
     fn set_volume(&mut self, amplitude: Amplitude, tween: Tween);
 }
